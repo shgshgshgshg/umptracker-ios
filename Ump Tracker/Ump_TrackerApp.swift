eec7743bd1810eefe2daecbb0f4d93436b2e3599
shgshgshgshg <75140487+shgshgshgshg@users.noreply.github.com>
2022-10-12 19:19:21 -0400

    Initial Commit

---
+++ Ump Tracker/Ump_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ump_TrackerApp.swift
+//  Ump Tracker
+//
+//  Created by Simon Gil on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Ump_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
