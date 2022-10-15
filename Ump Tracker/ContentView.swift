6f5c118dd68885d0dc5cd5cd1e055c22e150c4db
shgshgshgshg <75140487+shgshgshgshg@users.noreply.github.com>
2022-10-15 10:28:24 -0400

    built logic and basic UI

--- Ump Tracker/ContentView.swift
+++ Ump Tracker/ContentView.swift
@@ -8,12 +8,279 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Variables
+    @State var strikes : Int = 0
+    @State var balls : Int = 0
+    @State var outs : Int = 0
+    @State var inning : Int = 1
+    @State var top_inning : Bool = true
+    @State var HOME_TEAM : String = ""
+    @State var AWAY_TEAM : String = ""
+    @State var home_points : Int = 0
+    @State var away_points : Int = 0
+    
+    // Functions
+    
+    //      Functions for Strikes
+    func strike() -> Void {
+        strikes += 1
+        if (strikes >= 3) {
+            out()
+        }
+    }
+    
+    func strike_min() -> Void {
+        strikes -= 1
+        if (strikes <= 0) {
+            strikes = 0
+        }
+    }
+    //          End Function
+    
+    //      Function for Outs
+    func out() -> Void {
+        strikes = 0
+        balls = 0
+        outs += 1
+        if (outs >= 3) {
+            outs = 0
+            if (top_inning) {
+                top_inning = false
+            }
+            else {
+                top_inning = true
+                inning += 1
+            }
+        }
+    }
+    
+    func out_min() -> Void {
+        outs -= 1
+        if (outs <= 3) {
+            outs = 0
+        }
+    }
+    //         End Function
+    
+    //      Function for balls
+    func ball() -> Void {
+        balls += 1
+        if (balls >= 4) {
+            balls = 0
+            strikes = 0
+        }
+    }
+    
+    func ball_min() -> Void {
+        balls -= 1
+        if (balls <= 0) {
+            balls = 0
+        }
+    }
+    
+    //      Functions for innings
+    func inning_plus() -> Void {
+        if (top_inning == false) {
+            inning += 1
+            top_inning = true
+        }
+        else if (top_inning == true) {
+            top_inning = false
+        }
+    }
+    
+    func inning_min() -> Void {
+        if (top_inning == false) {
+            inning -= 1
+            top_inning = true
+        }
+        else {
+            top_inning = false
+        }
+        
+        if (inning <= 0) {
+            inning = 1
+        }
+    }
+    
+    //      Functions for Points
+    func home_pts_plus() -> Void {
+        home_points += 1
+    }
+    
+    func home_pts_min() -> Void {
+        home_points -= 1
+        if (home_points <= 0) {
+            home_points = 0
+        }
+    }
+    
+    func away_pts_plus() -> Void {
+        away_points += 1
+    }
+    
+    func away_pts_min() -> Void {
+        away_points -= 1
+        if (away_points <= 0) {
+            away_points = 0
+        }
+    }
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (alignment: .leading) { //VStack for inning
+            Text("Inning: \(inning)")
+                .font(.title2)
+                .fontWeight(.bold)
+            Text("Top of Inning: \(String(top_inning))")
+                .font(.title2)
+                .fontWeight(.bold)
+            Button(action: {
+                inning_plus()
+            }) {
+                Text("Inning +")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue)
+            }
+            Button(action: {
+                inning_min()
+            }) {
+                Text("Inning -")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+                
+            }
+            Text("")
+                .font(.title)
+        }
+        Divider()
+        VStack { //VStack for strikes
+            HStack { //Button HStack
+                Button(action: {
+                    strike()
+                }) {
+                    Text("Strike +")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
+                Text("|")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Button(action: {
+                    strike_min()
+                }) {
+                    Text("Strike -")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                }
+            }
+            Text("\(strikes)")
+                .font(.title)
+                .fontWeight(.bold)
+        }
+        VStack { //VStack for balls
+            HStack { //Button HStack
+                Button(action: {
+                    ball()
+                }) {
+                    Text("Ball +")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
+                Text("|")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Button(action: {
+                    ball_min()
+                }) {
+                    Text("Ball -")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                }
+            }
+            Text("\(balls)")
+                .font(.title)
+                .fontWeight(.bold)
+        }
+        VStack { //VStack for outs
+            HStack { //HStack for outs
+                Button(action: {
+                    out()
+                }) {
+                    Text("Out +")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
+                Text("|")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Button(action: {
+                    out_min()
+                }) {
+                    Text("Out -")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                }
+            }
+            Text("\(outs)")
+                .font(.title)
+                .fontWeight(.bold)
+        }
+        Divider()
+        VStack { //VStack for points
+            VStack { //For home team points
+                Text("Home:")
+                Text("\(home_points)")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Button(action: {
+                    home_pts_plus()
+                }) {
+                    Text("Home +")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                Button(action: {
+                    home_pts_min()
+                }) {
+                    Text("Home -")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                }
+                }
+            }
+            VStack { //For away team points
+                Text("Away:")
+                Text("\(away_points)")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Button(action: {
+                    away_pts_plus()
+                }) {
+                    Text("Away +")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                Button(action: {
+                    away_pts_min()
+                }) {
+                    Text("Away -")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                }
+                }
+            }
         }
         .padding()
     }
--- Ump Tracker/Ump_TrackerApp.swift
+++ Ump Tracker/Ump_TrackerApp.swift
@@ -1,14 +1,61 @@
-//
-//  Ump_TrackerApp.swift
-//  Ump Tracker
-//
-//  Created by Simon Gil on 10/12/22.
-//
+/*
+    Ump_TrackerApp.swift
+    Ump Tracker
+
+    Created by Simon Gil on 10/12/22.
+*/
 
 import SwiftUI
 
 @main
 struct Ump_TrackerApp: App {
+    //  Variables
+    @State var strikes : Int = 0
+    @State var balls : Int = 0
+    @State var outs : Int = 0
+    @State var inning : Int = 0
+    @State var top_inning : Bool = true
+    @State var HOME_TEAM : String = ""
+    @State var AWAY_TEAM : String = ""
+    
+    //  Functions
+    
+    //      Function for strikes
+    func strike() {
+        strikes += 1
+        if (strikes >= 3) {
+            out()
+        }
+    }
+    //          End Function
+    
+    //      Function for Outs
+    func out() {
+        strikes = 0
+        balls = 0
+        outs += 1
+        if (outs >= 3) {
+            if (top_inning) {
+                top_inning = false
+            }
+            else {
+                top_inning = true
+                inning += 1
+            }
+        }
+    }
+    //         End Function
+    
+    //      Function for balls
+    func ball() {
+        balls += 1
+        if (balls >= 4) {
+            balls = 0
+            strikes = 0
+        }
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
